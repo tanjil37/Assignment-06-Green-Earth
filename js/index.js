536f045d93ad0dff8e061e4a5db86b6dc99db0be
Tanjilul Islam <tanjilulislam37@gmail.com>
2025-09-13 05:31:43 +0600

    all functionality added

--- js/index.js
+++ js/index.js
@@ -54,26 +54,19 @@ const loadCategories = () => {
 
 const removeActive = () => {
     const lessonButtons = document.querySelectorAll(".categorie-btn")
-    //console.log(lessonButtons)
     lessonButtons.forEach((btn)=>btn.classList.remove("active"))
 
 }
 const loadPlantCategories = (id) =>{
-  //console.log(id);
   const url = `https://openapi.programming-hero.com/api/category/${id}`
     fetch(url)
     .then((res)=>res.json())
     .then((data)=>{
-      //console.log(data)
         removeActive();
-       
         const clickBtn = document.getElementById(`categorie-btn-${id}`);
-        //console.log(clickBtn)
         clickBtn.classList.add("active")
         displayAllPlantCategorie(data.plants);
     });
-
-
 }
 const loadPlantDetails = async (id) => {
   const url = `https://openapi.programming-hero.com/api/plant/${id}`;
@@ -81,8 +74,47 @@ const loadPlantDetails = async (id) => {
   const details = await res.json();
   displayPlanDetails(details.plants);
 };
+const priceCount = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  priceCountShow(details.plants)
+};
+const priceCountShow = (plants) =>{
+  alert(`${plants.name} has been added to cart`)
+  const cartContainer = document.getElementById("add-cart-container")
+  const cartDiv = document.createElement("div")
+  cartDiv.id = `cart-item-${plants.id}`
+  cartDiv.innerHTML= 
+  `
+             <!-- price -->
+                <div
+                  class="flex items-center bg-sky-50 justify-between p-2 my-2 rounded-lg"
+                >
+                  <div class="space-y-2">
+                    <h2>${plants.name}</h2>
+                    <p>৳${plants.price} </p>
+                  </div>
+                  <div>
+                    <i onclick="removeItem(${plants.id}, ${plants.price})"  class="fa-solid fa-xmark"></i>
+                  </div>
+                </div>
+  `  
+  cartContainer.append(cartDiv)
+  let total = parseInt(document.getElementById("total").innerText)
+  let finalTotal = total + plants.price;
+  document.getElementById("total").innerText = finalTotal
+}
+function removeItem(id, price) {
 
-
+  const item = document.getElementById(`cart-item-${id}`);
+  if (item) {
+    item.remove();
+  }
+  let total = parseInt(document.getElementById("total").innerText);
+  let finalTotal = total - price;
+  document.getElementById("total").innerText = finalTotal;
+}
 
 const displayPlanDetails = (plants) =>{
   const detailsBox = document.getElementById("details-container");
@@ -103,6 +135,7 @@ const displayPlanDetails = (plants) =>{
             </div>
   `;
   document.getElementById("plant_modal").showModal() 
+
 }
 const displayAllPlantCategorie = (plants) => {
   //console.log(plants)
@@ -119,7 +152,7 @@ const displayAllPlantCategorie = (plants) => {
                     alt=""
                   />
                   <div class="space-y-2 mt-2">
-                    <h2 onclick="loadPlantDetails(${plant.id})" class="text-sm font-semibold">${plant.name}</h2>
+                    <h2 onclick="loadPlantDetails(${plant.id})" class="text-sm font-semibold add-cart-btn-${plant.id}">${plant.name}</h2>
                     <p class="text-xs font-normal">
                      ${plant.description}
                     </p>
@@ -131,7 +164,7 @@ const displayAllPlantCategorie = (plants) => {
                     <span class="text-black font-semibold">৳${plant.price}</span>
                   </div>
                   <div>
-                    <button
+                    <button onclick="priceCount(${plant.id})"
                       class="mt-2 w-full px-3 py-2 bg-[#15803D] text-xs text-white rounded-full"
                     >
                       Add to Cart
@@ -143,31 +176,7 @@ const displayAllPlantCategorie = (plants) => {
     plantsContainer.append(cardDiv)
   }
 
-  //  wordContainer.innerHTML = " ";
-  //   if(words.length==0){
-  //       wordContainer.innerHTML= `
-  //       <div class="space-y-4 col-span-full text-center font-bangla">
-  //           <img class="mx-auto" src="./assets/alert-error.png" alt="">
-  //           <h2 class="font-normal text-sm text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</h2>
-  //           <p class="font-medium text-3xl text-[#292524]">নেক্সট Lesson এ যান</p>
-  //       </div>  
-  //       `;
-  //   }
-  //  words.forEach((word)=> {
-  //   const card = document.createElement('div');
-  //   card.innerHTML = `
-  //      <div class="bg-white text-center p-10 rounded-xl">
-  //       <h2 class="text-3xl font-bold">${word.word ? word.word : "কোন শব্দ পাওয়া যাই নি"}</h2>
-  //       <p class="text-xl mt-2 mb-3">Meaning /Pronounciation</p>
-  //       <div class="font-semibold text-3xl font-bangla">"${word.meaning ? word.meaning : "কোন অর্থ পাওয়া যাই নি"} / ${word.pronunciation ? word.pronunciation : "Pronounciation পাওয়া যাই নি"}"</div>
-  //       <div class="flex justify-between mt-6">
-  //         <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
-  //         <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
-  //       </div>
-  //     </div>
-  //   `
-  //   wordContainer.append(card)
-  //  })
+ 
 }
 
 
